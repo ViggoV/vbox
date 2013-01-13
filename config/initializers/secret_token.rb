a7e88500d4961b740a8191c8c53482b3870d20d2
Victor Eg Froelund <vefr86@gmail.com>
2013-01-13 17:13:09 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vbox::Application.config.secret_token = 'b4156ba7f91c9baeb4bb9d2cb2686134007ad22c0d3b1ee22ccae678bcb5d681f9ca30f030a3e780689a3c9f61f5255cb29d8b3232c26f9f8c5e5e3c9893af3b'
